64d92f66f96c53b13d7aac56e6a166fb44cae7f2
kumsalmuzsik <kumsalkumsalmuzik@gmail.com>
2024-10-21 17:25:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Silahanim21/Caylper",
+    "https://github.com/kumsalmuzsik/Caylper",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
